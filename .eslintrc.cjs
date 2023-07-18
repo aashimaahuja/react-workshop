5c3b5526be95fd1dd2bee86489c651e1ca9e1ff6
Aashima Ahuja <aashima.ahuja811@gmail.com>
2023-07-19 00:20:12 +0200

    storybook initial commit

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,42 @@
 /* eslint-env node */
 
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "prettier",
-    "plugin:react-hooks/recommended",
-  ],
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
-    semi: "warn",
-    "react/jsx-uses-react": 1,
-    "react/react-in-jsx-scope": 1,
-    "react-hooks/exhaustive-deps": 1,
-    "react/no-array-index-key": 1,
-    "react/prop-types": "off",
+  env: {
+    browser: true,
+    es2020: true,
   },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
+    'plugin:react-hooks/recommended',
+    'plugin:storybook/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: '18.2',
+    },
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      {
+        allowConstantExport: true,
+      },
+    ],
+    semi: 'warn',
+    'react/jsx-uses-react': 1,
+    'react/react-in-jsx-scope': 1,
+    'react-hooks/exhaustive-deps': 1,
+    'react/no-array-index-key': 1,
+    'react/prop-types': 'off',
+    'react/no-unescaped-entities': 0,
+  },
+  ignorePatterns: ['!.storybook'],
 };
---
+++ docs/storybook.md
@@ -0,0 +1,13 @@
+### Storybook
+
+**Why Storybook ?**
+
+Storybook provides a visual interface for previewing UI components and their state.
+
+Storybook makes it easier for designers and stakeholders to view the components.
+It is essential if you are creating a design system. You can put all your components in the storybook for devs to refer.
+
+### Installing storybook
+
+`npx storybook@latest init`
+`npm run storybook`
