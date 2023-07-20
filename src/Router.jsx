43e92374b5db01f7670a0a0900e1509d33103f00
Aashima Ahuja <aashima.ahuja811@gmail.com>
2023-07-21 00:27:13 +0200

    Code refactor

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-const Home = () => <h1>Home Page</h1>;
-const About = () => <h1>About Page</h1>;
+import { About } from './common/components/About';
+import { Home } from './common/components/Home';
 
 export const Router = () => {
   return (
