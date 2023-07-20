a650c5298f9c381c89b56b6f8dea8f55d20c892b
Aashima Ahuja <aashima.ahuja811@gmail.com>
2023-07-21 00:05:37 +0200

    Routing initial commit

---
+++ src/common/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div style={{ height: '50px', backgroundColor: 'black', width: '100vw' }}>
+      Header
+    </div>
+  );
+};
