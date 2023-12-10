673945d33f5576dfe44bca85d3fe986a809f1349
Aashima Ahuja <aashima.ahuja@fareharbor.com>
2023-12-10 11:39:40 +0530

    Modified header component

--- src/common/components/Header.jsx
+++ src/common/components/Header.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 export const Header = () => {
   return (
-    <div style={{ height: '50px', backgroundColor: 'black', width: '100vw' }}>
-      Header
+    <div
+      style={{
+        height: '50px',
+        backgroundColor: 'black',
+        width: '100vw',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '16px',
+      }}
+    >
+      <span style={{ color: 'white', marginLeft: '16px' }}>Home</span>
+
+      <span style={{ color: 'white' }}>About</span>
     </div>
   );
 };
