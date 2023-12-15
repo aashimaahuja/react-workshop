3be475f5d713237b31e45f86aba06e8ca9569ae9
Aashima Ahuja <aashima.ahuja811@gmail.com>
2023-12-15 15:53:18 +0100

    Adding routes file

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Home } from './pages/Home';
+
+export const routes = [
+  {
+    path: '/',
+    element: React.createElement(Home),
+  },
+  {
+    path: '/about',
+    element: React.createElement(Home),
+  },
+];
