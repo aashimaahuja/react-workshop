526fbd9630ea8cde8f405c0feea8a373fa985119
Aashima Ahuja <aashima.ahuja811@gmail.com>
2024-03-28 19:08:07 +0100

    removed unwanted code

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,26 +1,11 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useHttp } from '../hooks/useHttp';
-import { AddTodo } from './AddTodo';
-import { TodoListNav } from './TodoListNav';
 
 export const TodoList = () => {
   const { isLoading, get } = useHttp('https://jsonplaceholder.typicode.com');
   const [todos, setTodos] = useState([]);
-  const [activeTab, setActiveTab] = useState('userTodos');
-  const [input, setInput] = useState('');
-
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
-
-  const fetchTodos = useCallback(async () => {
-    setActiveTab('userTodos');
-    const todos = await get('/user/1/todos/');
-    setTodos(todos);
-  }, [get]);
 
   const fetchAllTodos = useCallback(async () => {
-    setActiveTab('allTodos');
     const todos = await get('/todos/');
     setTodos(todos);
   }, [get]);
@@ -29,42 +14,8 @@ export const TodoList = () => {
     fetchAllTodos();
   }, [fetchAllTodos]);
 
-  const addTodo = useCallback(async () => {
-    const options = {
-      headers: new Headers({
-        'Content-Type': 'application/json',
-      }),
-      body: {
-        completed: false,
-        id: todos.length + 1,
-        title: input,
-        userId: 1,
-      },
-    };
-
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos', {
-      method: 'POST',
-      body: JSON.stringify(options.body),
-      headers: options.headers,
-    });
-    const newTodo = await response.json();
-
-    setTodos([...todos, newTodo]);
-    setInput('');
-  }, [todos, input]);
-
   return (
     <main className="border rounded-lg border-slate-300 bg-white p-4 mr-4 w-1/2">
-      <AddTodo
-        addTodo={addTodo}
-        inputValue={input}
-        handleInputChange={handleInputChange}
-      />
-      <TodoListNav
-        fetchTodos={fetchTodos}
-        fetchAllTodos={fetchAllTodos}
-        activeTab={activeTab}
-      />
       <section className="grow">
         <h1 className="font-bold">Todos</h1>
         {isLoading ? (
